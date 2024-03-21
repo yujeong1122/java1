bae51a08b8941c6d6eb484c619992d66b8f68bdc
yujeong <jjjjj0407@gmail.com>
2024-03-21 14:23:05 +0900

    문자열을 switch를 이용해 숫자로 변환

---
+++ java1/src/Month2Int.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Month2Int {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String month;
+		
+		System.out.println("월의 이름을 입력하시오");
+		month = scan.next();
+		
+		switch (month)
+		{
+		case "january":
+			System.out.println("1");
+			break;
+		case "february":
+			System.out.println("2");
+			break;
+		case "march":
+			System.out.println("3");
+			break;
+		default:
+			System.out.println("0");
+			break;
+		}
+		
+	}
+
+}
