33bbe900edacdc3b23acbd550e18a9b11aa999c5
yujeong <jjjjj0407@gmail.com>
2024-03-21 13:37:19 +0900

    프로젝트트

---
+++ java1/src/EvenOdd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number;
+		
+		System.out.print("정수를 입력하시오.");
+		number = input.nextInt();
+		if (number%2==0)
+		{
+			System.out.println("입력된 정수는 짝수입니다.");
+		}
+		else
+		{
+			System.out.println("입력된 정수는 홀수입니다.");
+		}
+
+	}
+
+}
+
