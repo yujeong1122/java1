e57e0922d4f56cbcbfc26811dcd3dbb425784865
yujeong <jjjjj0407@gmail.com>
2024-03-25 15:05:41 +0900

    3주차 과제 do-whilw문 이용 '가'부터 '힣'까지 출력

---
+++ java1/src/KoreanChar.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+
+public class KoreanChar {
+
+	public static void main(String[] args) {
+		char mychar_g =  '\uAC00';
+		do {
+			System.out.println(mychar_g);
+			mychar_g = (char) (mychar_g+1);
+		} while(mychar_g <= '\uAE4B');
+		System.out.println("---------------------------");
+		
+		char mychar_gg =  '\uAE4C';
+		do {
+			System.out.println(mychar_gg);
+			mychar_gg = (char) (mychar_gg+1);
+		} while(mychar_gg <= '\uB097');
+		System.out.println("---------------------------");
+		
+		char mychar_n =  '\uB098';
+		do {
+			System.out.println(mychar_n);
+			mychar_n = (char) (mychar_n+1);
+		} while(mychar_n <= '\uB2E3');
+		System.out.println("---------------------------");
+		
+		char mychar_d =  '\uB2E4';
+		do {
+			System.out.println(mychar_d);
+			mychar_d = (char) (mychar_d+1);
+		} while(mychar_d <= '\uB52F');
+		System.out.println("---------------------------");
+		
+		char mychar_dd =  '\uB530';
+		do {
+			System.out.println(mychar_dd);
+			mychar_dd = (char) (mychar_dd+1);
+		} while(mychar_dd <= '\uB77B');
+		System.out.println("---------------------------");
+		
+		char mychar_l =  '\uB77C';
+		do {
+			System.out.println(mychar_l);
+			mychar_l = (char) (mychar_l+1);
+		} while(mychar_l <= '\uB9C7');
+		System.out.println("---------------------------");
+		
+		char mychar_m =  '\uB9C8';
+		do {
+			System.out.println(mychar_m);
+			mychar_m = (char) (mychar_m+1);
+		} while(mychar_m <= '\uBC13');
+		System.out.println("---------------------------");
+		
+		char mychar_b =  '\uBC14';
+		do {
+			System.out.println(mychar_b);
+			mychar_b = (char) (mychar_b+1);
+		} while(mychar_b <= '\uBE5F');
+		System.out.println("---------------------------");
+		
+		char mychar_bb =  '\uBE60';
+		do {
+			System.out.println(mychar_bb);
+			mychar_bb = (char) (mychar_bb+1);
+		} while(mychar_bb <= '\uC0AB');
+		System.out.println("---------------------------");
+		
+		char mychar_s =  '\uC0AC';
+		do {
+			System.out.println(mychar_s);
+			mychar_s = (char) (mychar_s+1);
+		} while(mychar_s <= '\uC2F7');
+		System.out.println("---------------------------");
+		
+		char mychar_ss =  '\uC2F8';
+		do {
+			System.out.println(mychar_ss);
+			mychar_ss = (char) (mychar_ss+1);
+		} while(mychar_ss <= '\uC543');
+		System.out.println("---------------------------");
+		
+		char mychar_o =  '\uC544';
+		do {
+			System.out.println(mychar_o);
+			mychar_o = (char) (mychar_o+1);
+		} while(mychar_o <= '\uC78F');
+		System.out.println("---------------------------");
+		
+		char mychar_j =  '\uC790';
+		do {
+			System.out.println(mychar_j);
+			mychar_j = (char) (mychar_j+1);
+		} while(mychar_j <= '\uC9DB');
+		System.out.println("---------------------------");
+		
+		char mychar_jj =  '\uC9DC';
+		do {
+			System.out.println(mychar_jj);
+			mychar_jj = (char) (mychar_jj+1);
+		} while(mychar_jj <= '\uCC27');
+		System.out.println("---------------------------");
+		
+		char mychar_c =  '\uCC28';
+		do {
+			System.out.println(mychar_c);
+			mychar_c = (char) (mychar_c+1);
+		} while(mychar_c <= '\uCE73');
+		System.out.println("---------------------------");
+		
+		char mychar_k =  '\uCE74';
+		do {
+			System.out.println(mychar_k);
+			mychar_k = (char) (mychar_k+1);
+		} while(mychar_k <= '\uD0BF');
+		System.out.println("---------------------------");
+		
+		char mychar_t = '\uD0C0';
+		do {
+			System.out.println(mychar_t);
+			mychar_t = (char) (mychar_t+1);
+		} while(mychar_t <= '\uD30B');
+		System.out.println("---------------------------");
+		
+		char mychar_p =  '\uD30C';
+		do {
+			System.out.println(mychar_p);
+			mychar_p = (char) (mychar_p+1);
+		} while(mychar_p <= '\uD557');
+		System.out.println("---------------------------");
+		
+		char mychar_h =  '\uD558';
+		do {
+			System.out.println(mychar_h);
+			mychar_h = (char) (mychar_h+1);
+		} while(mychar_h <= '\uD7A3');
+		System.out.println("---------------------------");
+		
+	}
+
+}
